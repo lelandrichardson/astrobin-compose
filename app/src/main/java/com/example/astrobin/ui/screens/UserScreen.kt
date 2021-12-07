dfd145d63cafd9fa41b4aa7a216f48e6181ee1ed
Daniel Santiago <danyboricua91@gmail.com>
2021-12-07 10:57:25 -0500

    Small first pass to User screen.

--- app/src/main/java/com/example/astrobin/ui/screens/UserScreen.kt
+++ app/src/main/java/com/example/astrobin/ui/screens/UserScreen.kt
@@ -1,10 +1,25 @@
 package com.example.astrobin.ui.screens
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.produceState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.ColorPainter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import coil.compose.rememberImagePainter
+import com.example.astrobin.api.AstroImage
 import com.example.astrobin.api.AstroUser
 import com.example.astrobin.api.LocalAstrobinApi
 
@@ -17,14 +32,94 @@ fun UserScreen(
   val data = produceState<AstroUser?>(null) {
     value = api.user(id)
   }.value
-  if (data == null) {
-    Text("Loading...")
-  } else {
-    Column {
-      Text(data.real_name)
-      Text(data.username)
-      Text("${data.following_count}")
-      Text("${data.following_count}")
+  UserScreenContent(data)
+}
+
+@Composable
+private fun UserScreenContent(user: AstroUser?) {
+  Column(
+    Modifier.fillMaxSize()
+  ) {
+    if (user == null) {
+      CircularProgressIndicator(
+        Modifier
+          .align(Alignment.CenterHorizontally)
+      )
+    } else {
+        val avatarPainter =
+            user.avatar?.let { rememberImagePainter(it) } ?: ColorPainter(Color.LightGray)
+        Image(
+            painter = avatarPainter,
+            contentDescription = "avatar",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .size(128.dp)
+                .clip(CircleShape)
+                .border(2.dp, Color.Black, CircleShape)
+        )
+        Text(
+            text = user.real_name,
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            fontSize = 32.sp
+        )
+        Text(
+            text = user.username,
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+        )
+        Row(
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+        ) {
+            Text("${user.following_count}")
+            Spacer(modifier = Modifier.width(4.dp))
+            Text("${user.following_count}")
+        }
+        Text("${user.about}")
+
+        // TODO: Gotta paginate!
+        val api = LocalAstrobinApi.current
+        val userImages = produceState(emptyList<AstroImage>()) {
+            value = api.imageSearch(
+                limit = 0,
+                offset = 0,
+                mapOf("username" to user.username)
+            ).objects
+        }.value
+        UserImages(userImages)
     }
   }
+}
+
+@Composable
+private fun UserImages(userImages: List<AstroImage>) {
+  for (image in userImages) {
+    Image(
+      painter = rememberImagePainter(image.url_regular),
+      contentDescription = image.dec
+    )
+  }
+}
+
+@Preview
+@Composable
+fun UserScreenPreview() {
+  val data = AstroUser(
+    id = 12345,
+    username = "DanyBoricua",
+    real_name = "Daniel Santiago",
+    followers_count = 100,
+    following_count = 10,
+    post_count = 5,
+    received_likes_count = 1000000,
+    image_count = 3,
+    about = "Cool Astro Dude",
+    hobbies = "Astro Gaming",
+    website = null,
+    job = "Sanitation Engineer",
+    date_joined = "2020-12-27T05:07:46.108323",
+    language = "es",
+    avatar = null,
+    resource_uri = "/api/v1/userprofile/12345/"
+  )
+  UserScreenContent(data)
 }
